c58670d3c83c3c0c029d8b754a868c1b015062db
Joao Marcelo Silva Tobler Mastrangelo <87100304+JMTobler@users.noreply.github.com>
2024-10-15 09:48:16 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplica(num1: number, num2: number) {
+    return num1 * num2
+}
+
+function saudacao(nome: string) {
+        return `Olá ${nome}`
+}
+
+let multi = multiplica(10, 10);
+let ola =saudacao('Joao');
